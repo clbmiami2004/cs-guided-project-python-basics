0e703ea71b6b1fc356f834499fb767957e9cdbde
Christian Lorenzo <clbmiami2004@gmail.com>
2020-10-27 19:14:30 -0700

    Demonstration 2

--- src/demonstration_01.py
+++ src/demonstration_01.py
@@ -9,5 +9,18 @@ Examples:
 - addition(7, 3) ➞ 10
 """
 def addition(a, b):
-    # Your code here
+    sum = a + b
+    
+    return sum
+    
+print(addition(3, 2)) # -> 5
+print(addition(-3, -6)) # -> -9
+print(addition(7, 3)) # -> 10
 
+# Optimized solution for the exercise above:
+def addition2(a, b):
+    return a + b
+
+print(addition2(3, 2)) # -> 5
+print(addition2(-3, -6)) # -> -9
+print(addition2(7, 3)) # -> 10
--- src/demonstration_02.py
+++ src/demonstration_02.py
@@ -8,6 +8,24 @@ Examples:
 - convert(3) ➞ 180
 - convert(2) ➞ 120
 """
-def convert(minutes):
-    # Your code here
+def convert(minutes) -> int:  # This is just like functions in Swift when returning a type.
+    seconds = minutes * 60
+    
+    return seconds
+    
+    
+print(convert(5))
+print(convert(3))
+print(convert(2))
+
+
+# Optimized implementation
+def convert2(minutes: int):
+    
+    return minutes * 60
+    
+    
+print(convert2(5))
+print(convert2(3))
+print(convert2(2))
 
